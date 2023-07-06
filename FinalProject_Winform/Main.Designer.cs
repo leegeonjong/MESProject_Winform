9226adeb73ed3d16e3c6c22d1a5edaa140936c25
leegeonjong <koka85260@gmail.com>
2023-07-06 17:26:24 +0900

    1

---
+++ FinalProject_Winform/Main.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace FinalProject_Winform
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(52, 152, 219);
+            pictureBox1.Location = new Point(0, -2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(801, 60);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(0, 53);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 404);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(884, 561);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox2);
+            Name = "Main";
+            Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ FinalProject_Winform/Repositories/LotRepository.cs
@@ -0,0 +1,24 @@
+﻿using FinalProject_Winform.Data;
+using FinalProject_Winform.Models.domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject_Winform.Repositories
+{
+    internal class LotRepository : ILotRepository
+    {
+        public Task<Lot> AddAsync(Lot Lot)
+        {
+          using FinalDbContext dbContext = new();
+            dbContext
+        }
+
+        public Task<IEnumerable<Lot>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
