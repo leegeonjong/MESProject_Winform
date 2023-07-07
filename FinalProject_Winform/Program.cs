b16f55aeb1b5f93b88fd2876169b6840d48ea6db
com7415 <qazx4740@naver.com>
2023-07-07 13:40:06 +0900

    ㅋㅋ

---
+++ FinalProject_Winform/Program.cs
@@ -0,0 +1,24 @@
+namespace FinalProject_Winform
+{
+    internal static class Program
+    {
+        //이건종
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+
+            Application.Run(new MainForm());
+
+
+            //Application.Run(new LoginForm());
+            // 나도 바쁘다고!
+
+        }
+    }
+}
