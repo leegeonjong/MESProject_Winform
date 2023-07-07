29b65e076fe4abb31fcc40ae1a547219c8c4d304
lateaksoo <ted26@naver.com>
2023-07-07 09:50:18 +0900

    rr

--- FinalProject_Winform/Form2.cs
+++ FinalProject_Winform/Form2.cs
@@ -16,5 +16,10 @@ namespace FinalProject_Winform
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
