526bd3fe819ad6b3d5c1c2a1b4edba2fb4f57a34
leegeonjon <koka85260@gmail.com>
2023-07-07 11:39:57 +0900

    ㅣㅐㅅ

--- FinalProject_Winform/Form/LOTForm.cs
+++ FinalProject_Winform/Form/LOTForm.cs
@@ -32,28 +32,31 @@ namespace FinalProject_Winform
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (comboBox_item.Text == "제품선택" && textBox_count.!Text.IsNullOrEmpty()) {
-                comboBox_item.Text = lbl_item.Text;
-                textBox_count.Text = lbl_count.Text;
+            if (comboBox_item.Text == "제품선택" || textBox_count.Text.IsNullOrEmpty())
+            {
                 MessageBox.Show("제품과 수량을 입력해주세요");
             }
+            else
+            {
+                comboBox_item.Text = lbl_item.Text;
+                textBox_count.Text = lbl_count.Text;
 
+                string barcodeText = lbl_barcode.Text.Trim();
+                if (string.IsNullOrEmpty(barcodeText)) { return; }
 
-            string barcodeText = lbl_barcode.Text.Trim();
-            if (string.IsNullOrEmpty(barcodeText)) { return; }
+                Barcode barcode = new();
 
-            Barcode barcode = new();
+                SKImage img = barcode.Encode(
+                    BarcodeStandard.Type.Ean13   // Barcode type
+                    , barcodeText                // 변환할 텍스트
+                    , new SKColor(0, 0, 0)       // fore color (바코드 색상) r,g,b
+                    , new SKColor(0, 0, 0, 0)   // back color (배경 색상)  r,g,b,a
+                    , (int)(picture_Barcode.Width * 0.8)   // width
+                    , (int)(picture_Barcode.Height * 0.8) // height
+                    );
 
-            SKImage img = barcode.Encode(
-                BarcodeStandard.Type.Ean13   // Barcode type
-                , barcodeText                // 변환할 텍스트
-                , new SKColor(0, 0, 0)       // fore color (바코드 색상) r,g,b
-                , new SKColor(0, 0, 0, 0)   // back color (배경 색상)  r,g,b,a
-                , (int)(picture_Barcode.Width * 0.8)   // width
-                , (int)(picture_Barcode.Height * 0.8) // height
-                );
-
-            picture_Barcode.Image = Image.FromStream(img.Encode().AsStream());
+                picture_Barcode.Image = Image.FromStream(img.Encode().AsStream());
+            }
         }
     }
 }
