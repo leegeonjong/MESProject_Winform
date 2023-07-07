efe82904976c5023ae6372f6c293c357b65b0036
leegeonjon <koka85260@gmail.com>
2023-07-07 13:15:13 +0900

    일단 커밋

--- FinalProject_Winform/Form/LOTForm.cs
+++ FinalProject_Winform/Form/LOTForm.cs
@@ -20,16 +20,8 @@ namespace FinalProject_Winform
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e) //지우면 날라감
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e) //지우면 날라감
-        {
-
-        }
-
+     
+ 
         private void button1_Click(object sender, EventArgs e)
         {
             if (comboBox_item.Text == "제품선택" || textBox_count.Text.IsNullOrEmpty())
@@ -40,7 +32,10 @@ namespace FinalProject_Winform
             {
                 lbl_item.Text = comboBox_item.Text;
                 lbl_count.Text = textBox_count.Text;
+                lbl_barcode.Text = makebarcode();
+       
             }
+                
 
                 string barcodeText = lbl_barcode.Text.Trim();
                 if (string.IsNullOrEmpty(barcodeText)) { return; }
@@ -52,13 +47,29 @@ namespace FinalProject_Winform
                     , barcodeText                // 변환할 텍스트
                     , new SKColor(0, 0, 0)       // fore color (바코드 색상) r,g,b
                     , new SKColor(0, 0, 0, 0)   // back color (배경 색상)  r,g,b,a
-                    , (int)(picture_Barcode.Width * 0.8)   // width
-                    , (int)(picture_Barcode.Height * 0.8) // height
+                    , (int)(picture_Barcode.Width)   // width
+                    , (int)(picture_Barcode.Height) // height
                     );
 
                 picture_Barcode.Image = Image.FromStream(img.Encode().AsStream());
             ////
             
+        }
+
+        private string makebarcode()
+        {
+            const int length = 12;
+            const string chars = "0123456789";
+            StringBuilder sb = new StringBuilder(length);
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(chars.Length);
+                sb.Append(chars[index]);
+            }
+
+            return sb.ToString();
         }
     }
 }
