424fd2c1f5c33222ebd7e2f6308193926ad1393f
com7415 <qazx4740@naver.com>
2023-07-12 13:45:29 +0900

    미로틱 탭2

--- FinalProject_Winform/Form/OrderForm.Designer.cs
+++ FinalProject_Winform/Form/OrderForm.Designer.cs
@@ -54,14 +54,18 @@
             label2 = new Label();
             tabPage2 = new TabPage();
             panel5 = new Panel();
+            button4 = new Button();
+            button3 = new Button();
+            dateTimePicker4 = new DateTimePicker();
+            label9 = new Label();
+            dateTimePicker3 = new DateTimePicker();
+            label8 = new Label();
             dataGridView1 = new DataGridView();
-            order_start = new DataGridViewTextBoxColumn();
-            order_account = new DataGridViewTextBoxColumn();
             order_item = new DataGridViewTextBoxColumn();
             order_count = new DataGridViewTextBoxColumn();
+            order_start = new DataGridViewTextBoxColumn();
             order_end = new DataGridViewTextBoxColumn();
-            label3 = new Label();
-            txtOrderName = new TextBox();
+            order_account = new DataGridViewTextBoxColumn();
             panel2.SuspendLayout();
             panel1.SuspendLayout();
             tabControl1.SuspendLayout();
@@ -370,35 +374,92 @@
             // panel5
             // 
             panel5.BackColor = Color.White;
+            panel5.Controls.Add(button4);
+            panel5.Controls.Add(button3);
+            panel5.Controls.Add(dateTimePicker4);
+            panel5.Controls.Add(label9);
+            panel5.Controls.Add(dateTimePicker3);
+            panel5.Controls.Add(label8);
             panel5.Controls.Add(dataGridView1);
             panel5.Location = new Point(6, 9);
             panel5.Name = "panel5";
             panel5.Size = new Size(1082, 606);
             panel5.TabIndex = 1;
             // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(52, 152, 219);
+            button4.FlatAppearance.BorderColor = Color.White;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(679, 80);
+            button4.Name = "button4";
+            button4.Size = new Size(132, 46);
+            button4.TabIndex = 20;
+            button4.Text = "검색";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(52, 152, 219);
+            button3.FlatAppearance.BorderColor = Color.White;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(679, 20);
+            button3.Name = "button3";
+            button3.Size = new Size(132, 46);
+            button3.TabIndex = 19;
+            button3.Text = "검색";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // dateTimePicker4
+            // 
+            dateTimePicker4.Location = new Point(463, 95);
+            dateTimePicker4.Name = "dateTimePicker4";
+            dateTimePicker4.Size = new Size(200, 23);
+            dateTimePicker4.TabIndex = 18;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("맑은 고딕", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(307, 88);
+            label9.Name = "label9";
+            label9.Size = new Size(152, 37);
+            label9.TabIndex = 17;
+            label9.Text = "마감일검색";
+            // 
+            // dateTimePicker3
+            // 
+            dateTimePicker3.Location = new Point(462, 35);
+            dateTimePicker3.Name = "dateTimePicker3";
+            dateTimePicker3.Size = new Size(200, 23);
+            dateTimePicker3.TabIndex = 16;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("맑은 고딕", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(304, 25);
+            label8.Name = "label8";
+            label8.Size = new Size(152, 37);
+            label8.TabIndex = 15;
+            label8.Text = "요청일검색";
+            // 
             // dataGridView1
             // 
+            dataGridView1.AllowUserToAddRows = false;
             dataGridView1.BackgroundColor = Color.White;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { order_start, order_account, order_item, order_count, order_end });
-            dataGridView1.Location = new Point(0, 187);
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { order_item, order_count, order_start, order_end, order_account });
+            dataGridView1.Dock = DockStyle.Bottom;
+            dataGridView1.Location = new Point(0, 157);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(1082, 416);
+            dataGridView1.Size = new Size(1082, 449);
             dataGridView1.TabIndex = 1;
-            // 
-            // order_start
-            // 
-            order_start.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-            order_start.HeaderText = "요청일";
-            order_start.Name = "order_start";
-            order_start.ReadOnly = true;
-            // 
-            // order_account
-            // 
-            order_account.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-            order_account.HeaderText = "거래처";
-            order_account.Name = "order_account";
             // 
             // order_item
             // 
@@ -414,30 +475,19 @@
             order_count.Name = "order_count";
             order_count.ReadOnly = true;
             // 
+            // order_start
+            // 
+            order_start.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            order_start.HeaderText = "요청일";
+            order_start.Name = "order_start";
+            order_start.ReadOnly = true;
+            // 
             // order_end
             // 
             order_end.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             order_end.HeaderText = "마감일";
             order_end.Name = "order_end";
             order_end.ReadOnly = true;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("맑은 고딕", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(581, 16);
-            label3.Name = "label3";
-            label3.Size = new Size(125, 47);
-            label3.TabIndex = 4;
-            label3.Text = "주문명";
-            // 
-            // txtOrderName
-            // 
-            txtOrderName.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            txtOrderName.Location = new Point(726, 21);
-            txtOrderName.Name = "txtOrderName";
-            txtOrderName.Size = new Size(265, 39);
-            txtOrderName.TabIndex = 5;
             // 
             // OrderForm
             // 
@@ -450,6 +500,7 @@
             Name = "OrderForm";
             Text = "OrderForm";
             FormClosed += OrderForm_FormClosed;
+            Load += OrderForm_Load;
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
             panel1.ResumeLayout(false);
@@ -461,6 +512,7 @@
             panel3.PerformLayout();
             tabPage2.ResumeLayout(false);
             panel5.ResumeLayout(false);
+            panel5.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
@@ -494,12 +546,9 @@
         private DateTimePicker dateTimePicker1;
         private Panel panel5;
         private DataGridView dataGridView1;
-        private DataGridViewTextBoxColumn order_start;
-        private DataGridViewTextBoxColumn order_account;
         private DataGridViewTextBoxColumn order_item;
         private DataGridViewTextBoxColumn order_count;
+        private DataGridViewTextBoxColumn order_start;
         private DataGridViewTextBoxColumn order_end;
-        private TextBox txtOrderName;
-        private Label label3;
     }
 }
--- FinalProject_Winform/Form/OrderForm.cs
+++ FinalProject_Winform/Form/OrderForm.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FinalProject_Winform.Data;
+using FinalProject_Winform.Models.domain;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,7 +19,19 @@ namespace FinalProject_Winform
         {
             InitializeComponent();
         }
+        DateTimePicker startdate;
+        DateTimePicker enddate;
+        DataGridView dgv;
 
+        private void OrderForm_Load(object sender, EventArgs e)
+        {
+            startdate = dateTimePicker1;
+            enddate = dateTimePicker2;
+            startdate.Value = DateTime.Now;
+            enddate.Value = DateTime.Now;
+            dgv = dataGridView1;
+            GridViewLoad();
+        }
         private void Button_Click(object sender, EventArgs e)
         {
             Button button = sender as Button;
@@ -40,11 +55,72 @@ namespace FinalProject_Winform
             this.Hide();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private async void button3_Click(object sender, EventArgs e)
         {
-            string account = txtAccount.Text;
-            string ordername = txtOrderName.Text;
+            using (FinalDbContext db = new())
+            {
+                var srt = startdate.Value;
+                var orders = await db.Orders.Where(x => x.Order_startDate == srt).ToListAsync();
+                dgv.Rows.Clear();
+                dgv.Refresh();
+                int i = 0;
+                foreach (var order in orders)
+                {
+                    dgv.Rows.Add();
+                    dgv.Rows[i].Cells["order_item"].Value = order.Item.Item_name;
+                    dgv.Rows[i].Cells["order_count"].Value = order.Order_count;
+                    dgv.Rows[i].Cells["order_start"].Value = order.Order_startDate;
+                    dgv.Rows[i].Cells["order_end"].Value = order.Order_endDate;
+                    dgv.Rows[i].Cells["order_account"].Value = order.Order_account;
+                    i++;
+                }
+            }
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            using (FinalDbContext db = new())
+            {
+                var edt = enddate.Value;
+                var orders = await db.Orders.Where(x => x.Order_endDate == edt).ToListAsync();
+                dgv.Rows.Clear();
+                dgv.Refresh();
+                int i = 0;
+                foreach (var order in orders)
+                {
+                    dgv.Rows.Add();
+                    dgv.Rows[i].Cells["order_item"].Value = order.Item.Item_name;
+                    dgv.Rows[i].Cells["order_count"].Value = order.Order_count;
+                    dgv.Rows[i].Cells["order_start"].Value = order.Order_startDate;
+                    dgv.Rows[i].Cells["order_end"].Value = order.Order_endDate;
+                    dgv.Rows[i].Cells["order_account"].Value = order.Order_account;
+                    i++;
+                }
+            }
+        }
+
+        private async void GridViewLoad()
+        {
+            using (FinalDbContext db = new())
+            {
+                var orders = await db.Orders.Include(x => x.Item).ToListAsync();
+                dgv.Rows.Clear();
+                dgv.Refresh();
+                int i = 0;
+                foreach (var order in orders)
+                {
+                    dgv.Rows.Add();
+                    dgv.Rows[i].Cells["order_item"].Value = order.Item.Item_name;
+                    dgv.Rows[i].Cells["order_count"].Value = order.Order_count;
+                    dgv.Rows[i].Cells["order_start"].Value = order.Order_startDate;
+                    dgv.Rows[i].Cells["order_end"].Value = order.Order_endDate;
+                    dgv.Rows[i].Cells["order_account"].Value = order.Order_account;
+                    i++;
+                }
+            }
 
         }
+
+
     }
 }
