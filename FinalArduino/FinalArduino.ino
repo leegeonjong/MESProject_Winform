ecbdaf62af49ea909d2cb29bc928ecaf2ef8a6f3
lateaksoo <ted26@naver.com>
2023-07-13 13:26:22 +0900

    dkendlsh

--- FinalArduino/FinalArduino.ino
+++ FinalArduino/FinalArduino.ino
@@ -4,20 +4,35 @@ int Process1Led2 = 53;
 int Process1Motor = 51;
 int Process1Sw = 50;
 
+int ProcessSw[] = { 22, 23, 24, 25, 26, 27 };  //설비 정지 버튼
+int LedRed[] = { 30, 32, 38, 41, 46, 49 };     //설비 정지
+int LedGreen[] = { 31, 34, 39, 42, 47, 50 };   //설비 가동중
+int LedYellow[] = { 33, 35, 40, 43, 48, 51 };  //설비 표현
+
+
 void setup() {
   Serial.begin(9600);  // Serial monitor 구동 전원입력
   pinMode(WaterSensor, INPUT);
 
-  pinMode(Process1Sw, INPUT_PULLUP);
-  pinMode(Process1Led1, OUTPUT);
-  pinMode(Process1Led2, OUTPUT);
-  pinMode(Process1Motor, OUTPUT);
+  for (int i = 0; i < 6; i++) {
+    pinMode(ProcessSw[i], INPUT_PULLUP);
+    pinMode(LedRed[i], OUTPUT);
+    pinMode(LedGreen[i], OUTPUT);
+    pinMode(LedYellow[i], OUTPUT);
+  }
 }
+
 void loop() {
-  action = "";   //공정상태
-  rest = "";     //나머지
-  process = "";  // 공정명
-  lotid = "";    // lotid
+  for (int i = 0; i < 6; i++) {
+    digitalWrite(LedRed[i], HIGH);
+    // digitalWrite(LedGreen[i], HIGH);
+    // digitalWrite(LedYellow[i], HIGH);
+  }
+
+  String action = "";   //공정상태
+  String rest = "";     //나머지
+  String process = "";  // 공정명
+  String lotid = "";    // lotid
   // $Run,Mix,213 명령 받고 이렇게 돌려주도록 한다
 
   if (Serial.available() > 0) {  // <- 수신 버퍼를 비우려면 while(Serial.available()) 사용해야 한다.  https://www.baldengineer.com/when-do-you-use-the-arduinos-to-use-serial-flush.html
@@ -29,20 +44,22 @@ void loop() {
       process = rest.substring(0, line.indexOf(","));
       lotid = rest.substring(line.indexOf(",") + 0);
       if (action == "Run") {
-        lcd.print("run");
-        Serial.println("$Recieve,lotstatus,lotid");
-        Serial.println(action);
-        Serial.println(process);
-        Serial.println(lotid);
+        SendRecieve(process, lotid);
+        SendStart(process, lotid);
+        // 프로세스 시작
+        // 10초 타이머 시작
+        SendEnd(process, lotid);
+      } else if (action == "On") {
+        SendOn(process, lotid);
+        for (int i = 0; i < 6; i++) {
+          digitalWrite(LedRed[i], HIGH);
+        }
+      } else if (action == "Off") {
+        SendOff(process, lotid);
+        for (int i = 0; i < 6; i++) {
+          digitalWrite(LedRed[i], LOW);
+        }
       }
-      else if (cmd == "On") {
-        digitalWrite(Process1Led1, HIGH);
-        digitalWrite(Process1Led2, LOW);
-      } else if (cmd == "Off") {
-        digitalWrite(Process1Led1, LOW);
-        digitalWrite(Process1Led2, HIGH);
-      }
-
     }
   }
 
