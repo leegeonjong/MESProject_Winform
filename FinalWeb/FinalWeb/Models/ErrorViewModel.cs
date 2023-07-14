0c41ed5943a7477c12d725142917d6a841b7c011
keemdonghoo <kiingdonghoo@gmail.com>
2023-07-14 15:29:36 +0900

    123

--- FinalWeb/FinalWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FinalWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
