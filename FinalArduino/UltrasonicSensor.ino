c4498d237d9caf803bc25d1ec1c82753b91cf98a
Lateaksoo <ted26@naver.com>
2023-07-31 17:29:44 +0900

    됐다

--- FinalArduino/UltrasonicSensor.ino
+++ FinalArduino/UltrasonicSensor.ino
@@ -5,11 +5,10 @@ int ECHO_Minus = 15;  //빼기 ECHO 핀 설정 (초음파 받는 핀)
 
 const int speedOfSound = 34300;
 
-
 bool minusToggle = false;
 bool plusToggle = false;
 
-void UltrasonicSensor(String process ,String lotId) {
+void UltrasonicSensor(String process, String lotId) {
   pinMode(TRIG_Plus, OUTPUT);
   pinMode(ECHO_Plus, INPUT);
 
@@ -25,13 +24,13 @@ void UltrasonicSensor(String process ,String lotId) {
 
 
   durationPlus = pulseIn(ECHO_Plus, HIGH);
- distancePlus = durationPlus * 17 / 1000;
+  distancePlus = durationPlus * 17 / 1000;
 
   if (distancePlus < 5 && distancePlus != 0) {
     if (Lot_Count >= 0 && !plusToggle) {
       plusToggle = true;
       Lot_Count++;
-      SendTestData(process,lotId ,String(Lot_Count));
+      SendTestData(process, lotId, String(Lot_Count));
       return;
     }
   } else {
@@ -39,25 +38,27 @@ void UltrasonicSensor(String process ,String lotId) {
   }
 }
 
-void UltrasonicSensorMinus(String process,String lotId) {
+void UltrasonicSensorMinus(String process, String lotId) {
   pinMode(TRIG_Minus, OUTPUT);
   pinMode(ECHO_Minus, INPUT);
 
   long durationMinus, distanceMinus;
 
   digitalWrite(TRIG_Minus, LOW);
+
   delayMicroseconds(2);
   digitalWrite(TRIG_Minus, HIGH);
+
   delayMicroseconds(10);
   digitalWrite(TRIG_Minus, LOW);
 
   durationMinus = pulseIn(ECHO_Minus, HIGH);
   distanceMinus = durationMinus * 17 / 1000;
 
-  if (distanceMinus < 3 && distanceMinus != 0) {
-    if (Lot_Count > 0 && !minusToggle) {
+  if (distanceMinus < 5 && distanceMinus != 0) {
+    if (Lot_Count >= 0 && !minusToggle) {
       minusToggle = true;
-      Lot_Count--;
+      Lot_Count++;
       SendTestData(process, lotId, String(Lot_Count));
     }
   } else {
